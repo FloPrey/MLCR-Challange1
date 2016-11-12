fc4db4a1ce5c0eb448154d68e5ca5b0fc26bf584
Milzi <mastermilz@web.de>
2016-11-12 14:47:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Analyzer import Analyzer
 analyzer = Analyzer()
 
 analyzer.loadData()
+analyzer.improveData(80, True)
+analyzer.createSets()
 analyzer.predictFloor()
 analyzer.predictLocation()
-
