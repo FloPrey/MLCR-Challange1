79763d01bf582a025ce4fe746d133eec76bc720f
Phil <zachpn@hotmail.com>
2016-11-12 05:03:09 +0100

    Added Feature selection

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from Analyzer import Analyzer
 analyzer = Analyzer()
 
 analyzer.loadData()
-analyzer.improveData()
+# analyzer.improveData(percentage for ap occurence, set values <-85 to -85)
+analyzer.improveData(70, True)
 analyzer.predictFloor()
 analyzer.predictLocation()
 
