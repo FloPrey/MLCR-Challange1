74ff8cef59e531d9c5f1a0a6e70a923978300ffc
Milzi <mastermilz@web.de>
2016-11-13 18:55:34 +0100

    Update main.py
    added classifier choice

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from Analyzer import Analyzer
 
 analyzer = Analyzer()
 
+randomForest = "RandomForest"
+knn = "KNN"
+naiveBayes = "NaiveBayes"
+svm = "SVM"
+
 analyzer.loadData()
+
+# you can change the optimization value - but we recommend 80%
+# more details can be found in the readme
 analyzer.improveData(80, True)
 analyzer.createSets()
-analyzer.predictFloor()
-analyzer.predictLocation()
+
+# please pass on the classifier of your choice - they can be different
+analyzer.predictFloor(randomForest)
+analyzer.predictLocation(randomForest)
